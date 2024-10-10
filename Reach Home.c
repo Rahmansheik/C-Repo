49f6d644190e9183863ead4f4886c8ebf55827ed
rahman <azeezurrahmansheik@gmail.com>
2024-10-10 21:33:36 +0530

    Uploading code: Reach Home.c

---
+++ Reach Home.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    int m=x*5;
+    if (m>=y)
+    {
+    printf("YES");
+    }
+    else
+    {
+    printf("NO");
+    }
+}
